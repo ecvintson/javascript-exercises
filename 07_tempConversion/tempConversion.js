75a7c393e1a46d794f6ba7dffea7fbf10e66edb6
Evan <ecvintson@gmail.com>
2022-04-26 19:10:05 -0400

    Completes 07 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function(fInput) {
+  var c;
+  c = (fInput - 32)*(5/9);
+  if((c.toFixed(1) - c.toFixed()) === 0){
+    return(parseFloat(c.toFixed()));
+  }
+  else{
+    return(parseFloat(c.toFixed(1)));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(cInput) {
+  var f;
+  f = ((cInput*(9/5))+32);
+  if((f.toFixed(1) - f.toFixed()) === 0){
+    return(parseFloat(f.toFixed()));
+  }
+  else{
+    return(parseFloat(f.toFixed(1)));
+  }
 };
 
 // Do not edit below this line
